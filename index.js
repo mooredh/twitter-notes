97fd693326dd88f9cc215b812b4f2df9af5a83a6
Moore <mooredh@gmail.com>
2018-06-07 02:11:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const url = require('url');
+const express = require('express');
+const authenticator = require('./authenticator');
+const config = require('./config.json')
+const app = express();
+
+// Add cookie parsing functionality to our express app
+app.use(require('cookie-parser'));
+
+// Take user to Twitter's login page
+app.get('/auth/twitter', authenticator.redirectToTwitterLoginPage);
+
+// This is the callback url that the user is redirected to after signing in
+app.get(url.parse(config.oauth_callback).path, (req, res) => {
+    authenticator.authenticate(req, res, (err) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(401);
+        } else {
+            res.send("Authentication successful");
+        }
+    });
+});
+
+// Start listening for requests 
+app.listen(config.port, () => {
+    console.log(`Listening on port ${port}`);
+});
